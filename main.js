d53b35b4956702f5109906e3f39a44859602b04a
baxtiyorov2013 <baxtiyorov2013>
2024-04-04 10:55:57 +0500

    first commit

---
+++ main.js
@@ -0,0 +1,11 @@
+let a = Number(prompt("1-sonni kiriting"));
+let b = Number(prompt("2-sonni kiriting"));
+
+function Kota_kichkina() {
+    if (a > b) {
+        alert("eng katta son " + a);
+    }else if (a < b) {
+        alert("eng katta son " + b);
+    }
+}
+Kota_kichkina();
